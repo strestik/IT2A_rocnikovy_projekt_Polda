c93b390e8bef26009cd3ebd6660226c51498c67b
strestik <matejstrestik@gmail.com>
2026-05-15 00:52:09 +0200

    update

---
+++ Polda/Polda/IT2A_rocnikovy_projekt_Polda/IT2A_rocnikovy_projekt_Polda/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace IT2A_rocnikovy_projekt_Polda
+{
+    public partial class MainWindow : Window
+    {
+        Random rnd = new Random();
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+            Pankrac pankrac = new Pankrac("Pankrac", 0.5, 0.5, new Image(), "Pankrac");
+        }
+
+        private List<Hotspot> polygons = new List<Hotspot>()
+        {
+            new Hotspot("Jumbo", 3, 0.9, "Jumbo"),
+            new Hotspot("Postel", 1.0, 0.5, "Postel")
+        };
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DrawPoints();
+        }
+
+        void DrawPoints()
+        {
+            OverlayCanvas.Children.Clear();
+
+            foreach (var poly in polygons)
+            {
+                poly.polygon.MouseDown -= Polygon_MouseDown;
+                poly.polygon.MouseDown += Polygon_MouseDown;
+                poly.polygon.Tag = poly;
+
+                System.Windows.Point Point1 = new System.Windows.Point(456 * poly.XPercent, 870 * poly.YPercent);
+                System.Windows.Point Point2 = new System.Windows.Point(545 * poly.XPercent, 570 * poly.YPercent);
+                System.Windows.Point Point3 = new System.Windows.Point(50 * poly.XPercent, 550 * poly.YPercent);
+                PointCollection myPointCollection = new PointCollection();
+                myPointCollection.Add(Point1);
+                myPointCollection.Add(Point2);
+                myPointCollection.Add(Point3);
+                poly.polygon.Points = myPointCollection;
+                OverlayCanvas.Children.Add(poly.polygon);
+            }
+        }
+
+        private void Polygon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Polygon btn = sender as Polygon;
+            Hotspot point = btn.Tag as Hotspot;
+            MessageBox.Show(point.init);
+
+        }
+
+        private void MapImage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            DrawPoints();
+        }
+
+
+        private void MapImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var pos = e.GetPosition(MapImage);
+
+            double xPercent = pos.X;
+            double yPercent = pos.Y;
+
+            MessageBox.Show($"{xPercent:F4} , {yPercent:F4}");
+            MessageBox.Show($"Tam nic není.");
+        }
+    }
+}
---
+++ Polda/Polda/IT2A_rocnikovy_projekt_Polda/IT2A_rocnikovy_projekt_Polda/Pankrac.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace IT2A_rocnikovy_projekt_Polda
+{
+    internal class Pankrac
+    {
+        public string Name { get; set; }
+        public double posX { get; set; }
+        public double posY { get; set; }
+        public string init { get; set; }
+        public Image pankrac { get; set; }
+
+
+        public Pankrac(string name, double xPercent, double yPercent, Image img, string item = "Empty")
+        {
+            Name = name;
+            posX = xPercent;
+            posY = yPercent;
+            pankrac = img;
+            init = item;
+        }
+
+        public void refresh()
+        {
+            Canvas.SetLeft(pankrac, posX);
+            Canvas.SetTop(pankrac, posY);
+        }
+
+        public void move(double targetX, double targetY)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(16); // ~60fps
+            timer.Tick += (s, e) =>
+            {
+
+                double deltaX = targetX - posX;
+                double deltaY = targetY - posY;
+                double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+                if (distance < 3)
+                {
+                    posX = targetX;
+                    posY = targetY;
+                    refresh();
+                    timer.Stop();
+                    return;
+                }
+                double speed = 3; // pixels per tick
+                posX += (deltaX / distance) * speed;
+                posY += (deltaY / distance) * speed;
+                refresh();
+            };
+            timer.Start();
+        }
+    }
+}
